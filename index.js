1093e02dbf18b3a90d4dc93ae1463b23c54eec69
mulwajoseph <mulwajose.jm@gmail.com>
2023-07-25 10:57:05 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+//Function expression divide
+const divide = (num1 ,num2) => num1/num2;
+divide(2000/100)
+
+//arrow function called square 
+const num = [4,9,16,25]
+const square = num => (num * num)
+num;
+//to add
+
+const add = (num1, num2) => (num1 + num2)
+num;
